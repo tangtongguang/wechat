dc6937fb6fe5c68359eff4e2e15b07b00fa95aa6
yusanguang <tangtongguang@sina.com>
2020-12-23 15:47:44 +0800

    modify  return code

--- middleWares/index.js
+++ middleWares/index.js
@@ -10,7 +10,7 @@ const codeMiddleWare = require('./code')
 module.exports = (app) => {
     // error 放在所有中间件之前，就可以捕获它们所有的同步或者异步代码中抛出的异常
     // codeMiddleWare 放在所有中间件之后
-    const middleWares = [errorMiddleWare, codeMiddleWare]
+    const middleWares = [errorMiddleWare]
 
     middleWares.forEach((middleware) => {
         app.use(middleware)
